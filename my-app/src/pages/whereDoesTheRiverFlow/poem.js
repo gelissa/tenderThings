a7ee86aa1045a76435cf715145864fb736885a71
gelissa <gelissaleveille@yahoo.com>
2020-01-07 12:36:52 -0500

    added poem three

---
+++ my-app/src/pages/whereDoesTheRiverFlow/poem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css";
+
+function PoemThree(){
+    
+        return (
+            <div className="poem">
+                <h1 className="title">
+                    WHERE DOES THE RIVER FLOW?
+                </h1>
+                <p className="p">
+                    from whence cometh my help <br />
+                    when grey fills my mind <br />
+                    when my eyes lose sight <br />
+                    of what is, what could be — <br />
+                    for who is listening <br />
+                    to the distance which calls <br />
+                    out, it's a voice not unlike my <br />
+                    own, who was, who could have <br />
+                    been — <br />
+                    I seek a homecoming <br />
+                    drifting ever farther <br />
+                    into the grey — who <br />
+                    is out there <br />
+                    which might offer rest to <br />
+                    a soul walking on aching feet? <br />
+                    from which hill <br />
+                    dost flow the <br />
+                    river styx? <br />
+                </p>
+            </div>
+        )
+    };
+
+export default PoemTwo;
